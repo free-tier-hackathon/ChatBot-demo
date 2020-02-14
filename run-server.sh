ecf0ce4e992607333cb07d8a590abf17e6b114cf
Gundega <gundega.dekena@gmail.com>
2020-02-15 01:04:21 +0200

    Update run-server.sh

--- run-server.sh
+++ run-server.sh
@@ -3,4 +3,4 @@
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
     (cd example_app; python manage.py createsuperuser --no-input)
 fi
-(cd example_app; gunicorn wsgi.py --user www-data --bind 0.0.0.0:8010 --workers 1) &
+(cd example_app; gunicorn example_app.wsgi --user www-data --bind 0.0.0.0:8080 --workers 1) &
