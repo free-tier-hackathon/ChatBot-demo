87d6a9b28d1dd96f31bce6aa488c777305726185
Gundega <gundega.dekena@gmail.com>
2020-02-15 13:19:11 +0200

    Update run-server.sh

--- run-server.sh
+++ run-server.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
-# start-server.sh
+
+NLTK_DATA="/var/www/nltk_data"
+export $NLTK_DATA
+
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
     (python manage.py createsuperuser --no-input)
 fi
+
 (gunicorn example_app.wsgi --user www-data --bind 0.0.0.0:8000 --workers 1) &
 nginx -g "daemon off;"
